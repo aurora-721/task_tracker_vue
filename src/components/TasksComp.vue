38c5de9b0bb17c209943577418506f3b8ed37ff7
aurora-721 <aurora.makovac@gmail.com>
2022-07-29 01:54:00 +0200

    basic list

---
+++ src/components/TasksComp.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-for="task in tasks" :key="task.id">
+    <TaskComp :task="task" />
+  </div>
+</template>
+
+<script>
+import TaskComp from '@/components/TaskComp.vue'
+
+export default {
+  name: 'TasksComp',
+  props: {
+    tasks: Array
+  },
+  components: {
+    TaskComp
+  }
+}
+</script>
+
+<style scope>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <HeaderComp title="something" showAddTask="false" />
+    <TasksComp :tasks="tasks"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderComp from '@/components/HeaderComp.vue'
+import TasksComp from '@/components/TasksComp.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HeaderComp,
+    TasksComp
+  },
+  data () {
+    return {
+      tasks: []
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'March 1st at 2:30pm',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Doctor Appointment',
+        day: 'March 2nd at 2:30pm',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Pharmacy Appointment',
+        day: 'March 23rd at 2:30pm',
+        reminder: true,
+      },
+    ]
   }
+
 }
 </script>
